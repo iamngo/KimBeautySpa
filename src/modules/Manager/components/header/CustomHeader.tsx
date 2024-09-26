2a40e124cdb5be668506206ae2568890d428df92
iamngo <88821216+iamngo@users.noreply.github.com>
2024-09-26 16:48:18 +0700

    layout manage

---
+++ src/modules/Manager/components/header/CustomHeader.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Layout, Button, Select, Avatar } from "antd";
+import "./Header.scss";
+import { FaBell, FaCodeBranch, FaFacebookMessenger } from "react-icons/fa";
+const { Header } = Layout;
+const { Option } = Select;
+
+const CustomHeader: React.FC = () => {
+  return (
+    <Header className="custom-header">
+      <div className="branch-info">
+      <Select defaultValue="Chi nhánh số 1" style={{ width: 200 }} suffixIcon={<FaCodeBranch />}>
+          <Option value="1">Chi nhánh số 1</Option>
+          <Option value="2">Chi nhánh số 2</Option>
+        </Select>
+      </div>
+      <div className="actions">
+        <Button icon={<FaBell />} type="text" />
+        <Button icon={<FaFacebookMessenger />} type="text" />
+        <Avatar
+          size="large"
+        />
+      </div>
+    </Header>
+  );
+};
+
+export default CustomHeader;
--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,4 @@
 export const LOGIN = "/login";
 export const HOME = "/";
+export const MANAGER = "/manager";
+export const DASHBOARD = "dashboard";
