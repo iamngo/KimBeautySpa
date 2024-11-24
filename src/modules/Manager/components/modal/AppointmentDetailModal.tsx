192fb44fb055dd9acfd1b0ab6dc4ba270d3af07c
iamngo <88821216+iamngo@users.noreply.github.com>
2024-11-24 16:45:12 +0700

    add && update appointment detail modal

--- src/modules/Manager/components/modal/AppointmentDetailModal.tsx
+++ src/modules/Manager/components/modal/AppointmentDetailModal.tsx
@@ -1,13 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  Modal,
-  Form,
-  Select,
-  Button,
-  message,
-  Row,
-  Col,
-} from "antd";
+import { Modal, Form, Select, Button, message, Row, Col } from "antd";
 import moment from "moment";
 import { MODE } from "../../../../utils/constants";
 import {
@@ -19,6 +11,7 @@ import {
   getEmployeeByDateTime,
   getPricesByForeignKeyId,
   getServiceByCategory,
+  updateAppointmentDetail,
 } from "../../../../services/api";
 import { useBranch } from "../../../../hooks/branchContext";
 import { Appointment } from "../../types";
@@ -41,11 +34,7 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
   onSuccess,
 }) => {
   const [form] = Form.useForm();
-  const [selectedService, setSelectedService] = useState<number | null>(null);
-  const [serviceList, setServiceList] = useState<any[]>([]);
-  const [staffList, setStaffList] = useState<any[]>([]);
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
-  const [timeSlots, setTimeSlots] = useState<any[]>([]);
   const [selectedServiceId, setSelectedServiceId] = useState<number | null>(
     null
   );
@@ -65,26 +54,58 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
   const currentDateTime = new Date();
   const currentDate = currentDateTime.toISOString().split("T")[0]; // Format: YYYY-MM-DD
   const currentHour = currentDateTime.getHours();
-  const dateTimeString = `${currentDate} ${currentHour.toString().padStart(2, '0')}:00:00`;
+  const dateTimeString = `${currentDate} ${currentHour
+    .toString()
+    .padStart(2, "0")}:00:00`;
   const token = localStorage.getItem("accessToken");
 
-
   useEffect(() => {
-    if (visible) {
+    
       getServiceCategory();
-      fetchProduct();
       if (mode === MODE.ADD) {
         form.resetFields();
         form.setFieldsValue({ status: "confirmed" });
       } else if (mode === MODE.EDIT && appointmentData) {
-        const formattedData = {
-          ...appointmentData,
-          appointmentDate: moment(appointmentData.appointmentDate),
-        };
-        form.setFieldsValue(formattedData);
+        setSelectedCategory(appointmentData.category);
+        if (appointmentData.category === "services") {
+          setSelectedServiceId(appointmentData.foreignKeyId);
+
+          // Tìm categoryId từ foreignKeyId trong servicesByCategory
+          const categoryId = Object.keys(servicesByCategory).find(
+            (categoryId) =>
+              servicesByCategory[categoryId]?.some(
+                (service) => service.id === appointmentData.foreignKeyId
+              )
+          );
+
+          if (categoryId) {
+            setSelectedCategoryId(Number(categoryId));
+            // Lấy room từ categoryId
+            getCategoryServiceById(Number(categoryId))
+              .then((response) => {
+                setRoom(response?.data);
+                const formattedData = {
+                  ...appointmentData,
+                  service: `${appointmentData.foreignKeyId} - ${appointmentData.expense}`,
+                  bedId: appointmentData.bedId,
+                  employeeId: appointmentData.employeeId,
+                };
+                form.setFieldsValue(formattedData);
+              })
+              .catch((error) => {
+                console.error("Error fetching category:", error);
+              });
+          }
+        } else {
+          const formattedData = {
+            ...appointmentData,
+            productId: `${appointmentData.foreignKeyId} - ${appointmentData.expense}`,
+          };
+          form.setFieldsValue(formattedData);
+        }
       }
-    }
-  }, [visible, mode, appointmentData, form]);
+    
+  }, [visible, mode, appointmentData]);
 
   useEffect(() => {
     if (selectedCategoryId) {
@@ -97,19 +118,30 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
       getBedByServiceAndDate();
     }
   }, [room, dateTimeString]);
+  useEffect(() => {
+    if (
+      selectedCategory === "products" ||
+      (mode === MODE.EDIT && appointmentData?.category === "products")
+    ) {
+      fetchProduct();
+    }
+  }, [selectedCategory, mode]);
 
   useEffect(() => {
     getEmployees();
-  }, [selectedDate, selectedServiceId]);
-
+  }, [visible, dateTimeString, selectedServiceId]);
 
   const getEmployees = async () => {
     const response = await getEmployeeByDateTime(
       Number(branchId),
       dateTimeString
     );
-    const filteredEmployees = response?.data?.filter(emp => emp.role === "employee");
+    const filteredEmployees = response?.data?.filter(
+      (emp) => emp.role === "employee"
+    );
     setEmployees(filteredEmployees);
+    console.log(dateTimeString);
+    
   };
 
   const fetchProduct = async () => {
@@ -118,69 +150,71 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
       const productsWithPrices = await Promise.all(
         response.data.map(async (product) => {
           const priceResponse = await getPricesByForeignKeyId(product.id);
-          const price = priceResponse.data[0]?.specialPrice || 0; 
+          const price = priceResponse.data[0]?.specialPrice || 0;
           return {
             ...product,
-            price: price
+            price: price,
           };
         })
       );
-
-      console.log("Products with their prices:", productsWithPrices);
       setProducts(productsWithPrices);
-
     } catch (error) {
       console.error("Error fetching products with prices:", error);
       message.error("Có lỗi khi lấy thông tin sản phẩm!");
     }
   };
 
-
   const onFinish = async (values: any) => {
     try {
+      let foreignKeyId, expense;
+      if (values.category === "services") {
+        const [serviceId, price] = values.service.split(" - ");
+        foreignKeyId = Number(serviceId);
+        expense = Number(price);
+      } else if (values.category === "products") {
+        const [productId, price] = values.productId.split(" - ");
+        foreignKeyId = Number(productId);
+        expense = Number(price);
+      }
+
+      const appointmentData = {
+        appointmentId: appointmentId,
+        category: values.category,
+        status: values.status,
+        foreignKeyId: foreignKeyId,
+        expense: expense,
+        ...(values.category === "services" && {
+          bedId: values.bedId,
+          employeeId: values.employeeId,
+        }),
+      };
+
       if (mode === MODE.ADD) {
-        let foreignKeyId, expense;
-
-        if (values.category === 'services') {
-          // Xử lý cho dịch vụ
-          const [serviceId, price] = values.service.split(" - ");
-          foreignKeyId = Number(serviceId);
-          expense = Number(price);
-        } else if (values.category === 'products') {
-          // Xử lý cho sản phẩm
-          const [productId, price] = values.productId.split(" - ");
-          foreignKeyId = Number(productId);
-          expense = Number(price);
-        }
-
-        const appointmentData = {
-          appointmentId: appointmentId,
-          category: values.category,
-          status: values.status,
-          foreignKeyId: foreignKeyId,
-          expense: expense,
-          // Chỉ thêm bedId và employeeId nếu là dịch vụ
-          ...(values.category === 'services' && {
-            bedId: values.bedId,
-            employeeId: values.employeeId,
-          })
-        };
-
-        console.log("Data to submit:", appointmentData);
         const response = await createAppointmentDetail(token, appointmentData);
-        console.log(response);
-        
         if (response.data) {
           message.success("Thêm chi tiết thành công!");
-          onSuccess?.(); // Gọi callback để refresh table
+          onSuccess?.();
           setVisible(false);
-        } else {
-          message.error("Thêm chi tiết thất bại!");
+        }
+      } else if (mode === MODE.EDIT && appointmentData?.id) {
+        const response = await updateAppointmentDetail(
+          token,
+          appointmentData?.id,
+          appointmentData
+        );
+        if (response.data) {
+          message.success("Cập nhật chi tiết thành công!");
+          onSuccess?.();
+          setVisible(false);
         }
       }
     } catch (error) {
       console.error("Error submitting form:", error);
-      message.error("Có lỗi xảy ra khi thêm chi tiết!");
+      message.error(
+        mode === MODE.ADD
+          ? "Có lỗi xảy ra khi thêm chi tiết!"
+          : "Có lỗi xảy ra khi cập nhật chi tiết!"
+      );
     }
   };
 
@@ -224,17 +258,15 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
   const handleServiceChange = (value: string) => {
     const [serviceId, price] = value.split(" - ");
     setSelectedServiceId(Number(serviceId));
-    setSelectedService(Number(serviceId));
     setExpense(Number(price));
-    
+
     // Tìm categoryId
-    const categoryId = Object.keys(servicesByCategory).find(
-      (categoryId) =>
-        servicesByCategory[categoryId].some(
-          (service) => service.id === Number(serviceId)
-        )
+    const categoryId = Object.keys(servicesByCategory).find((categoryId) =>
+      servicesByCategory[categoryId].some(
+        (service) => service.id === Number(serviceId)
+      )
     );
-    
+
     setSelectedCategoryId(Number(categoryId));
   };
 
@@ -282,7 +314,8 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
         </Row>
 
         {/* Dòng 2: Sản phẩm */}
-        {selectedCategory === "products" && (
+        {(selectedCategory === "products" ||
+          (mode === MODE.EDIT && appointmentData?.category === "products")) && (
           <Row gutter={16}>
             <Col span={24}>
               <Form.Item
@@ -294,7 +327,9 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
                   placeholder="Chọn sản phẩm"
                   options={products?.map((product) => ({
                     value: `${product.id} - ${product.price}`,
-                    label: `${product.name} - ${product.price.toLocaleString('vi-VN')}đ`
+                    label: `${product.name} - ${product.price.toLocaleString(
+                      "vi-VN"
+                    )}đ`,
                   }))}
                 />
               </Form.Item>
@@ -303,89 +338,96 @@ const AppointmentDetailModal: React.FC<UpdateAppointmentModalProps> = ({
         )}
 
         {/* Dòng 3: Dịch vụ */}
-        {selectedCategory === "services" && (
-          <Row gutter={16}>
-            <Col span={24}>
-              <Form.Item
-                label="Chọn dịch vụ:"
-                name="service"
-                rules={[{ required: true, message: "Vui lòng chọn dịch vụ" }]}
-              >
-                <Select
-                  placeholder="Chọn dịch vụ"
-                  onChange={handleServiceChange}
+        {(selectedCategory === "services" ||
+          (mode === MODE.EDIT && appointmentData?.category === "services")) && (
+          <>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item
+                  label="Chọn dịch vụ:"
+                  name="service"
+                  rules={[{ required: true, message: "Vui lòng chọn dịch vụ" }]}
                 >
-                  {serviceCategory?.map((category) => (
-                    <Select.OptGroup key={category.id} label={category.name}>
-                      {servicesByCategory[category.id]?.map((service) => (
-                        <Select.Option
-                          key={service.id}
-                          value={`${service.id} - ${service.specialPrice}`}
-                        >
-                          {service.name} -{" "}
-                          {service.specialPrice.toLocaleString("vi-VN", {
-                            style: "currency",
-                            currency: "VND",
-                          })}
-                        </Select.Option>
-                      ))}
-                    </Select.OptGroup>
-                  ))}
-                </Select>
-              </Form.Item>
-            </Col>
-          </Row>
-        )}
+                  <Select
+                    placeholder="Chọn dịch vụ"
+                    onChange={handleServiceChange}
+                  >
+                    {serviceCategory?.map((category) => (
+                      <Select.OptGroup key={category.id} label={category.name}>
+                        {servicesByCategory[category.id]?.map((service) => (
+                          <Select.Option
+                            key={service.id}
+                            value={`${service.id} - ${service.specialPrice}`}
+                          >
+                            {service.name} -{" "}
+                            {service.specialPrice.toLocaleString("vi-VN", {
+                              style: "currency",
+                              currency: "VND",
+                            })}
+                          </Select.Option>
+                        ))}
+                      </Select.OptGroup>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
 
-        {/* Dòng 4: Giường và Nhân viên - chỉ hiển thị khi đã chọn dịch vụ */}
-        {selectedCategory === "services" && selectedServiceId && (
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                name="bedId"
-                label="Giường"
-                rules={[{ required: true, message: "Vui lòng chọn giường!" }]}
-              >
-                <Select
-                  placeholder="Chọn giường"
-                  options={bed?.map((bed) => ({
-                    value: bed.id,
-                    label: bed.name,
-                  }))}
-                />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="employeeId"
-                label="Nhân viên"
-                rules={[{ required: true, message: "Vui lòng chọn nhân viên!" }]}
-              >
-                <Select
-                  placeholder="Chọn nhân viên"
-                  options={employees?.map((employee) => ({
-                    value: employee.id,
-                    label: employee.fullName,
-                  }))}
-                />
-              </Form.Item>
-            </Col>
-          </Row>
+            {/* Dòng 4: Giường và Nhân viên */}
+            {(selectedServiceId ||
+              (mode === MODE.EDIT &&
+                appointmentData?.category === "services")) && (
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                    name="bedId"
+                    label="Giường"
+                    rules={[
+                      { required: true, message: "Vui lòng chọn giường!" },
+                    ]}
+                  >
+                    <Select
+                      placeholder="Chọn giường"
+                      options={bed?.map((bed) => ({
+                        value: bed.id,
+                        label: bed.name,
+                      }))}
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    name="employeeId"
+                    label="Nhân viên"
+                    rules={[
+                      { required: true, message: "Vui lòng chọn nhân viên!" },
+                    ]}
+                  >
+                    <Select
+                      placeholder="Chọn nhân viên"
+                      options={employees?.map((employee) => ({
+                        value: employee.id,
+                        label: employee.fullName,
+                      }))}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+            )}
+          </>
         )}
         <Form.Item>
-        <Row justify="end" gutter={8}>
-          <Col>
-            <Button onClick={() => setVisible(false)}>
-              Hủy
-            </Button>
-          </Col>
-          <Col>
-            <Button type="primary" htmlType="submit">
-              {mode === MODE.ADD ? "Thêm" : "Cập nhật"}
-            </Button>
-          </Col>
-        </Row>
-      </Form.Item>
+          <Row justify="end" gutter={8}>
+            <Col>
+              <Button onClick={() => setVisible(false)}>Hủy</Button>
+            </Col>
+            <Col>
+              <Button type="primary" htmlType="submit">
+                {mode === MODE.ADD ? "Thêm" : "Cập nhật"}
+              </Button>
+            </Col>
+          </Row>
+        </Form.Item>
       </Form>
     </Modal>
   );
